ee2b6b36c4f6d380c907802ecac44a82b0f45744
alexanderbuc <abuc@ucsc.edu>
2015-05-12 17:12:29 -0700

    Initial changes to the code
    Trying to set up bfs

--- p6_analysis.py
+++ p6_analysis.py
@@ -9,17 +9,24 @@ def analyze(design):
     queue = []
     visited = []
     # TODO: fill in this function, populating the ANALYSIS dict
-    init = sim.get_state_initial_state()
+    init = sim.get_initial_state()
     moves = sim.get_moves()
     next_state = sim.get_next_state(init, moves[0])
-    position, abilities = next_state # or None if character dies
-    i, j = position
-
     queue.append((init, [init]))
     visited.append(init)
-
-    raise NotImplementedError
+    while queue:
+        (prev,path) = queue.pop(0)
+        for each in moves:
+            next_state = sim.get_next_state(prev, each)
+            if next_state not in ANALYSIS and next_state is not None:
+                if next_state not in visited:
+                    ANALYSIS[next_state] = path
+                    queue.append((next_state, path + [next_state]))
+                    visited.append(next_state)
 
 def inspect((i,j), draw_line):
     # TODO: use ANALYSIS and (i,j) draw some lines
+     for each in ANALYSIS.keys():
+        if i is each[0][0] and j is each[0][1]:
+            path = ANALYSIS[each]
     raise NotImplementedError
